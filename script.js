10ca7624b9f5088a63b11a7a3e5bad8fd6bd993d
makoto <makoto.makoto@gmail.com>
2023-07-05 17:24:35 -0300

    alteração

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 function validationForm(){
-    let validationCode = document.forms['register']['cod'].value;
-    let validationTitle = document.forms['register']['title'].value;
-    let validationDirector = document.forms['register']['director'].value;
-    let validationDate = document.forms['register']['date'].value;
-    let validationRadio = document.forms['register']['radio'].value;
-    let validationActor = document.forms['register']['actor'].value;
-    let validationNota = document.forms['register']['nota'].value;
+    let validationCod = document.forms["register"]["cod"].value;
+    let validationTitle = document.forms["register"]["title"].value;
+    let validationDirector = document.forms["register"]["director"].value;
+    let validationDate = document.forms["register"]["date"].value;
+    let validationRadio = document.forms["register"]["radio"].value;
+    let validationActor = document.forms["register"]["actor"].value;
+    let validationNota = document.forms["register"]["nota"].value;
 
-    if (validationCod == ""){
-        alert("O código deve ser preenchido");
+    if (validationCod == "") {
+        alert("O Código deve ser preenchido");
         return false;
     }
-    if (validationTitle == ""){
-        alert("O título deve ser preenchido");
+    if (validationTitle == "") {
+        alert("O titulo deve ser preenchido");
         return false;
     }
-    if (validationDirector == ""){
-        alert("O cdiretor deve ser preenchido");
+    if (validationDirector == "") {
+        alert("O Nome do diretor deve ser preenchido");
         return false;
     }
-    if (validationDate == ""){
+    if (validationDate == "") {
         alert("A data deve ser preenchida");
         return false;
     }    
-    if (validationRadio == ""){
-        alert("A gênero deve ser preenchido");
+    if (validationRadio == "") {
+        alert("O Gênero deve ser preenchido");
         return false;
     }
     if (validationActor == ""){
-        alert("O ator deve ser preenchido");
+        alert("O Nome do ator deve ser preenchido");
         return false;
     }
     if (validationNota == ""){
-        alert("A nota deve ser preenchida");
+        alert("A Nota deve ser preenchida");
         return false;
     }
-    console.log("A validação ocorreu de forma correta")
+    alert("A validação ocorreu de forma correta");
 }
 
-function insert(){
+function insert() {
     let cod = document.forms["register"]["cod"].value;
     let title = document.forms["register"]["title"].value;
     let director = document.forms["register"]["director"].value;
@@ -50,7 +50,7 @@ function insert(){
     let insert = window.document.getElementById("insertRow");
 
     insert.innerHTML = `
-    <th scope='row'>${cod}</th>
+    <th scope="row">${cod}</th>
     <td>${title}</td>
     <td>${director}</td>
     <td>${date}</td>
